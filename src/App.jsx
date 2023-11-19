25ec887407a6fc1e622a27c09f415154396e7339
Chris DeHaan <christopherwdehaan@gmail.com>
2023-11-19 13:06:16 -0500

    built out header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,14 @@ import { useState } from 'react'
 import {Outlet} from 'react-router-dom'
 import Navbar from './components/Nav'
 
-function App() {
+import './App.css'
 
+function App() {
 
   return (
     <>
-    <div className='row col-11 mx-auto'>
-      <div className='col-4'>
-        <p className='display-6'>Christopher DeHaan</p>
-      </div>
-      <div className='col-8 '>
-        <Navbar />
-      </div>
-    </div>
-      <main>
+      <Navbar/>
+      <main className='col-10 mx-auto'>
         <Outlet/>
       </main>
     </>
