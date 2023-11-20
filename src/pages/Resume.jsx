bc0bf2eb309c829f3c6613857bc5f3b49a1b3538
Chris DeHaan <christopherwdehaan@gmail.com>
2023-11-19 22:09:57 -0500

    added Resume layout and CSS

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,32 @@
 export default function Resume() {
-    return(
+    return (
         <div>
-        <h1 className="display-6 text-center m-4">This is my Resume</h1>
-        <p>
-            Lorem, ipsum dolor.
-        </p>
-    </div>
+            <h1 className="display-6 text-center m-4 title-custom">Resume</h1>
+            <div className="col-10 mx-auto mb-5 title-custom">
+                <div className="mb-5 text-center">
+                    <button className="btn btn-custom">Download Resume Here</button>
+                </div>
+                <h2>Front-End Proficiencies</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Responsive Design</li>
+                    <li>Tailwind</li>
+                    <li>Bootstrap</li>
+                    <li>React</li>
+                </ul>
+                <h3>Back-End Proficiencies</h3>
+                <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL, Sequelize</li>
+                    <li>MongoDB, Mongoose</li>
+                    <li>REST</li>
+                    <li>GraphQL</li>
+                </ul>
+            </div>
+        </div>
     )
 }
