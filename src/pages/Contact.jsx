c63db9ef3e700b173cc842b3329205ad7de29aef
Chris DeHaan <christopherwdehaan@gmail.com>
2023-11-19 21:44:32 -0500

    css and layout for contact page completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,26 @@
+import './Contact.css'
+
 export default function Contact() {
-    return(
+    return (
         <div>
-        <h1 className="display-6 text-center m-4">This is my Contact Me</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam velit ipsam error aperiam, placeat et aliquam quae harum!
-        </p>
-    </div>
+            <h1 className="display-6 text-center m-4 title-custom">Contact Me</h1>
+            <form className='col-10 mx-auto'>
+                <div className="col-6 mx-auto my-3">
+                    <label className='form-label col-3 fs-5 title-custom'>Name:</label>
+                    <input type='text' className='form-control form-custom' />
+                </div>
+                <div className="col-6 mx-auto my-3 fs-5 title-custom">
+                    <label className='form-label col-3'>Email:</label>
+                    <input type='text' className='form-control form-custom' />
+                </div>
+                <div className="col-6 mx-auto my-3 fs-5 title-custom">
+                    <label className='form-label col-3'>Message:</label>
+                    <textarea type='text' className='form-control form-custom' rows='5' />
+                </div>
+                <div className='col-6 mx-auto text-center mb-5'>
+                    <button className="btn btn-custom ">Submit</button>
+                </div>
+            </form>
+        </div>
     )
 }
