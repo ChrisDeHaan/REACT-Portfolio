bf7db5b2a208e094e2e08b1cc91f986632ed6ca6
Chris DeHaan <christopherwdehaan@gmail.com>
2023-11-21 16:40:58 -0500

    added tutor suggestions, fixed portfolio, added donwload

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,64 +1,104 @@
 import { useState } from 'react'
 
-import './Contact.css'
-
 export default function Contact() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
+    const [contactInfo, setContactInfo] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
 
-    function getElId (element, message) {document.getElementById(element).textContent=message}
-    function getInputId (element, message) {document.getElementById(element).value=message}
-    function clearFields () {
+    function getElId(element, message) { document.getElementById(element).textContent = message }
+    function getInputId(element, message) { document.getElementById(element).value = message }
+    function clearFields() {
         getElId('nameEl', '')
         getElId('emailEl', '')
         getElId('messageEl', '')
         getElId('submissionEl', '')
     }
 
-    const formValidation = () => {
-        clearFields()
-        if (name === '') {getElId('nameEl', 'Name is required. I need some way to address you!')}
-        if (email === '') {getElId('emailEl', 'Email is required. How can I contact you back?')}
-        if (message === '') {getElId('messageEl', 'Message is required.')}
-        if (!(name === '') && !(email === '') && !(message === '')) {
-            setName('')
-            setEmail('')
-            setMessage('')
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+    const validateForm = (e) => {
+        e.preventDefault();
+        clearFields();
+        if (contactInfo.name === '') { getElId('nameEl', 'Name is required. I need some way to address you!') }
+        if (contactInfo.email === '') { getElId('emailEl', 'Email is required. How can I contact you back?') }
+        if (contactInfo.message === '') { getElId('messageEl', 'Message is required.') }
+        if (!(contactInfo.name === '') && !(contactInfo.email === '') && !(contactInfo.message === '')) {
             getInputId('nameInput', '')
             getInputId('emailInput', '')
             getInputId('messageInput', '')
             getElId('submissionEl', "Thanks for your submission. I'll get back to you as soon as possible!")
-        } else {
-            return
         }
+    }
 
+    const handleBlur = (e) => {
+        if (e.target.name == "email") {
+            if (validateEmail(e.target.value) === null) {
+                getElId('emailEl', 'A valid email is required. How can I contact you back?')
+            }
+        } else if (!e.target.value.length) {
+            if (e.target.name === "name") {
+                getElId('nameEl', 'Name is required. I need some way to address you!')
+            }
+            if (e.target.name === "message") {
+                getElId('messageEl', 'Message is required.')
+            }
+        }
+    }
+
+    const handleChange = (e) => {
+        setContactInfo({
+            ...contactInfo,
+            [e.target.name]: e.target.value
+        })
+        if ((e.target.name === 'name')) {
+            if (!e.target.value.length) {
+                return
+            } else { getElId('nameEl', '') }
+        }
+        if ((e.target.name === 'email')) {
+            if (!e.target.value.length) {
+                return
+            } else { getElId('emailEl', '') }
+        }
+        if ((e.target.name === 'message')) {
+            if (!e.target.value.length) {
+                return
+            } else { getElId('messageEl', '') }
+        }
     }
 
     return (
         <div>
             <h1 className="display-6 text-center m-4 title-custom">Contact Me</h1>
             <div className='text-center fs-5' id='submissionEl'></div>
-            <form className='col-10 mx-auto'>
+            <form className='col-10 mx-auto' onSubmit={validateForm}>
                 <div className="col-12 col-md-7 mx-auto my-3">
                     <label className='form-label col-3 fs-4 title-custom'>Name:</label>
-                    <input type='text' className='form-control form-custom' onChange={(e) => setName(e.target.value)} id='nameInput'/>
+                    <input type='text' className='form-control form-custom' onBlur={handleBlur} onChange={handleChange} id='nameInput' name='name' />
                     <div className='fs-6 m-2 text-danger' id='nameEl'></div>
                 </div>
                 <div className="col-12 col-md-7 mx-auto my-3 fs-4 title-custom">
                     <label className='form-label col-3'>Email:</label>
-                    <input type='email' className='form-control form-custom' onChange={(e) => setEmail(e.target.value)} id='emailInput'/>
+                    <input type='email' className='form-control form-custom' onBlur={handleBlur} onChange={handleChange} id='emailInput' name="email" />
                     <div className='fs-6 m-2 text-danger' id='emailEl'></div>
                 </div>
                 <div className="col-12 col-md-7 mx-auto my-3 fs-4 title-custom">
                     <label className='form-label col-3'>Message:</label>
-                    <textarea type='text' className='form-control form-custom' rows='5' onChange={(e) => setMessage(e.target.value)} id='messageInput'/>
+                    <textarea type='text' className='form-control form-custom' rows='5' onBlur={handleBlur} onChange={handleChange} id='messageInput' name="message" />
                     <div className='fs-6 m-2 text-danger' id='messageEl'></div>
                 </div>
+                <div className='col-12 col-md-7 mx-auto text-center mb-5'>
+                    <button className="btn btn-custom" id='btnEl' >Submit</button>
+                </div>
             </form>
-            <div className='col-12 col-md-7 mx-auto text-center mb-5'>
-                <button className="btn btn-custom" id='btnEl' onClick={formValidation}>Submit</button>
-            </div>
+
         </div>
     )
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,10 @@
-import './Portfolio.css'
-
 import NasaSite from '../assets/NASA-API.png'
 import HealthcareSite from '../assets/project-2.png'
 import Jate from '../assets/JATE.png'
 import WeatherApp from '../assets/weather-app.png'
 import PasswordGen from '../assets/password-generator.png'
 import QuizApp from '../assets/quiz-app.png'
+import GithubLogo from '../assets/github-mark.png'
 
 export default function Portfolio() {
     return (
@@ -14,34 +13,82 @@ export default function Portfolio() {
 
             <div className='row col-10 mx-auto'>
                 <div className='col-12 col-md-6 my-4'>
+                    <div className='text-center fs-4 title-custom'>
+                        <p>NASA APIs</p>
+                    </div>
                     <a href="https://chrisdehaan.github.io/Team-SCCRM/" target="_blank">
                         <img src={NasaSite} className='imgEl' />
                     </a>
+                    <div className='text-center mt-2'>
+                        <a href='https://github.com/ChrisDeHaan/Team-SCCRM' target='_blank'>
+                            <img src={GithubLogo} className='imgRepo' />
+                        </a>
+                    </div>
                 </div>
                 <div className='col-12 col-md-6 my-4'>
+                    <div className='text-center fs-4 title-custom'>
+                        <p>Doctor Portal</p>
+                    </div>
                     <a href="https://sccrm-healthcare-bbafb87ca81b.herokuapp.com/" target="_blank">
                         <img src={HealthcareSite} className='imgEl' />
                     </a>
+                    <div className='text-center mt-2'>
+                        <a href='https://github.com/Elrond-Hubbard/Team-SCCRM-2' target='_blank'>
+                            <img src={GithubLogo} className='imgRepo' />
+                        </a>
+                    </div>
                 </div>
                 <div className='col-12 col-md-6 my-4'>
+                    <div className='text-center fs-4 title-custom'>
+                        <p>Just Another Note Taker</p>
+                    </div>
                     <a href="https://just-another-note-taker-cwd-fa89b7859e8d.herokuapp.com/" target="_blank">
                         <img src={Jate} className='imgEl' />
                     </a>
+                    <div className='text-center mt-2'>
+                        <a href='https://github.com/ChrisDeHaan/PWA-Text-Editor' target='_blank'>
+                            <img src={GithubLogo} className='imgRepo' />
+                        </a>
+                    </div>
                 </div>
                 <div className='col-12 col-md-6 my-4'>
+                    <div className='text-center fs-4 title-custom'>
+                        <p>Better Weather</p>
+                    </div>
                     <a href="https://chrisdehaan.github.io/Weather-App/" target="_blank">
                         <img src={WeatherApp} className='imgEl' />
                     </a>
+                    <div className='text-center mt-2'>
+                        <a href='https://github.com/ChrisDeHaan/Weather-App' target='_blank'>
+                            <img src={GithubLogo} className='imgRepo' />
+                        </a>
+                    </div>
                 </div>
                 <div className='col-12 col-md-6 my-4'>
+                    <div className='text-center fs-4 title-custom'>
+                        <p>Random Password Generator</p>
+                    </div>
                     <a href="https://chrisdehaan.github.io/Password-Generator/" target="_blank">
                         <img src={PasswordGen} className='imgEl' />
                     </a>
+                    <div className='text-center mt-2'>
+                        <a href='https://github.com/ChrisDeHaan/Password-Generator' target='_blank'>
+                            <img src={GithubLogo} className='imgRepo' />
+                        </a>
+                    </div>
                 </div>
                 <div className='col-12 col-md-6 my-4'>
+                    <div className='text-center fs-4 title-custom'>
+                        <p>Coding Quiz</p>
+                    </div>
                     <a href="https://chrisdehaan.github.io/Timed-Quiz/" target="_blank">
                         <img src={QuizApp} className='imgEl' />
                     </a>
+                    <div className='text-center mt-2'>
+                        <a href='https://github.com/ChrisDeHaan/Timed-Quiz' target='_blank'>
+                            <img src={GithubLogo} className='imgRepo' />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
